1cf1b936d59e17672cf5a9d7932a5238fc317d3a
Devendra Rajput <devenrajput12@gmail.com>
2026-03-14 11:41:34 +0530

    Largest number of 3 numbers

---
+++ Programs/Find_Largest_number.py
@@ -0,0 +1,12 @@
+num1 = float(input("Enter 1st number: "))
+num2 = float(input("Enter 2nd number: "))
+num3 = float(input("Enter 3rd number: "))
+
+if num1 >= num2 and num1 >= num3:
+    largest = num1
+elif num2 >= num1 and num2 >= num3:
+    largest = num2
+else:
+    largest = num3
+
+print(f"Largest number is {largest}")
