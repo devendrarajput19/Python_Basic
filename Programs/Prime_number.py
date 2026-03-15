3f5b49b559997c2f1bc6fa3ceee2b11af5b400bb
Devendra Rajput <devenrajput12@gmail.com>
2026-03-15 12:21:25 +0530

    Prime using function

--- Programs/Prime_number.py
+++ Programs/Prime_number.py
@@ -1,15 +1,31 @@
-num = int(input("Enter a number: "))
+#num = int(input("Enter a number: "))
 
-if num <= 1:
-    print(f"{num} is not a Prime")
-else:
-    is_Prime = True
+# if num <= 1:
+#     print(f"{num} is not a Prime")
+# else:
+#     is_Prime = True
+#     for i in range(2, int(num ** 0.5) + 1):
+#         if num % i == 0:
+#             is_prime = False
+#             break
+
+#     if is_Prime:
+#         print(f"{num} is Prime")
+#     else:
+#         print(f"{num} is not Prime")
+
+# Program using Function
+
+def is_Prime(num):
+    if num <= 1:
+        return False
     for i in range(2, int(num ** 0.5) + 1):
         if num % i == 0:
-            is_prime = False
-            break
-
-    if is_Prime:
-        print(f"{num} is Prime")
-    else:
-        print(f"{num} is not Prime")
+            return False
+    return True
+    
+num = int(input("Enter a number: "))
+if is_Prime(num):
+    print(f"{num} is Prime")
+else:
+    print(f"{num} is not Prime")
