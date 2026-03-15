faa08b4f074aed8f58b0478e5411b95ccdf927c7
Devendra Rajput <devenrajput12@gmail.com>
2026-03-15 12:17:19 +0530

    to check number is Prime

---
+++ Programs/Prime_number.py
@@ -0,0 +1,15 @@
+num = int(input("Enter a number: "))
+
+if num <= 1:
+    print(f"{num} is not a Prime")
+else:
+    is_Prime = True
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            is_prime = False
+            break
+
+    if is_Prime:
+        print(f"{num} is Prime")
+    else:
+        print(f"{num} is not Prime")
