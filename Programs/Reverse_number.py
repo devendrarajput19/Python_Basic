b2b4b8c664139ba29d3f80d0f8664391ccd5f5c9
Devendra Rajput <devenrajput12@gmail.com>
2026-03-17 14:03:21 +0530

    using reverse function and join

--- Programs/Reverse_number.py
+++ Programs/Reverse_number.py
@@ -20,3 +20,13 @@ num = int(input("Enter a number: "))
 reversed_num = int(str(num)[::-1])
 print(f"Original number is {num}")
 print(f"Reversed number is {reversed_num}")
+
+#reverse using Reversed() and join()
+num = input("Enter a number: ")
+reversed_num = "".join(reversed(num))  #this will only work for string not integer
+#It returns an iterator (a sequence you can loop through)
+# reversed(num) → ['3', '2', '1']  (conceptually)
+#"".join(...) = This joins all characters into a single string
+
+print(f"Original number is {num}")
+print(f"Reversed number is {reversed_num}")
