2a2e062c15afd2c251b287b10e7c86e26d5e9fac
Devendra Rajput <devenrajput12@gmail.com>
2026-03-18 20:00:15 +0530

    Palindrome using Function

--- Programs/Palindrome.py
+++ Programs/Palindrome.py
@@ -7,17 +7,32 @@
 
 #using a loop
 
+# num = int(input("Enter a number: "))
+
+# orginal_num = num
+# reversed_num = 0
+
+# while num > 0:
+#     digit = num % 10
+#     reversed_num = reversed_num * 10 + digit
+#     num //= 10
+
+# if orginal_num == reversed_num:
+#     print(f"{orginal_num} is Palindrome")
+# else:
+#     print(f"{orginal_num} is not Palindrome")
+
+
+#using Function
+
+def is_palindrome(num):
+    original = str(num)
+    return original == original[::-1]
+
 num = int(input("Enter a number: "))
 
-orginal_num = num
-reversed_num = 0
-
-while num > 0:
-    digit = num % 10
-    reversed_num = reversed_num * 10 + digit
-    num //= 10
-
-if orginal_num == reversed_num:
-    print(f"{orginal_num} is Palindrome")
+if is_palindrome(num):
+    print(f"{num} is Palindrome")
 else:
-    print(f"{orginal_num} is not Palindrome")
+    print(f"{num} is not Palindrome")
+    
