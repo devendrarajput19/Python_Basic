d2f4baac653e5e624660d73b077d2c4e583b354d
Devendra Rajput <devenrajput12@gmail.com>
2026-03-18 19:30:51 +0530

    Palindrome using iterator

--- Programs/Palindrome.py
+++ Programs/Palindrome.py
@@ -1,7 +1,23 @@
-num = input("Enter a number: ")  #this will consider number as string
+# num = input("Enter a number: ")  #this will consider number as string
 
-if num == num[::-1]:    #num[::-1] reverses the value and returns the string
-    print(f"{num} is Palindrome")
+# if num == num[::-1]:    #num[::-1] reverses the value and returns the string
+#     print(f"{num} is Palindrome")
+# else:
+#     print(f"{num} is not Palindrome")
+
+#using a loop
+
+num = int(input("Enter a number: "))
+
+orginal_num = num
+reversed_num = 0
+
+while num > 0:
+    digit = num % 10
+    reversed_num = reversed_num * 10 + digit
+    num //= 10
+
+if orginal_num == reversed_num:
+    print(f"{orginal_num} is Palindrome")
 else:
-    print(f"{num} is not Palindrome")
-
+    print(f"{orginal_num} is not Palindrome")
