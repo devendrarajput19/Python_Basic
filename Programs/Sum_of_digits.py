f30bad2eb46d1b2c1c764c510c889bdc4b2b08ec
Devendra Rajput <devenrajput12@gmail.com>
2026-03-20 11:59:05 +0530

    Sum of digits using map and sum function

--- Programs/Sum_of_digits.py
+++ Programs/Sum_of_digits.py
@@ -1,13 +1,20 @@
-num = int(input("Enter a number: "))
+# 1 - Using Loop
 
-orginal = 0
-digit_sum = 0
+# num = int(input("Enter a number: "))
 
-while num > 0:
-    digit = num % 10 #this gives you Quotient
-    digit_sum += digit
-    num //= 10   #ths removes last digit of the number
+# orginal = 0
+# digit_sum = 0
 
-print(f"Number = {orginal}")
-print(f"Sum of digits = {digit_sum}")
+# while num > 0:
+#     digit = num % 10 #this gives you Quotient
+#     digit_sum += digit
+#     num //= 10   #ths removes last digit of the number
+
+# print(f"Number = {orginal}")
+# print(f"Sum of digits = {digit_sum}")
+
+# 2 - Using sum() and map() function
+
+num = input("Enter a number: ")
+digit_sum = sum(map(int, num))  #map(int, num) - returns list of numbers with comma. and then it sum up the number
 
