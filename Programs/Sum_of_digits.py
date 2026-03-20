a008249faa36504d98f3667192bf02b3e26707d8
Devendra Rajput <devenrajput12@gmail.com>
2026-03-20 12:03:31 +0530

    Sum of digits using Recursion

--- Programs/Sum_of_digits.py
+++ Programs/Sum_of_digits.py
@@ -15,6 +15,23 @@
 
 # 2 - Using sum() and map() function
 
-num = input("Enter a number: ")
-digit_sum = sum(map(int, num))  #map(int, num) - returns list of numbers with comma. and then it sum up the number
+# num = input("Enter a number: ")
+# digit_sum = sum(map(int, num))  #map(int, num) - returns list of numbers with comma. and then it sum up the number
+
+# print(f"Number = {num}")
+# print(f"Sum is {digit_sum}")
+
+# Using Recursion
+
+def digit_sum(num):
+    if num == 0:
+        return 0
+    else:
+        return num % 10 + digit_sum(num // 10)
+
+num = int(input("Enter a number: "))
+
+print(f"Number = {num}")
+print(f"Sum of digits = {digit_sum(num)}")
+
 
