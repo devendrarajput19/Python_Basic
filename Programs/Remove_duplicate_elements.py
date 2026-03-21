a28e6cb6b6f02696372d97cff5b7c9d7061ebcb9
Devendra Rajput <devenrajput12@gmail.com>
2026-03-21 16:28:11 +0530

    Remove duplicate entries using loop

--- Programs/Remove_duplicate_elements.py
+++ Programs/Remove_duplicate_elements.py
@@ -1,10 +1,26 @@
-numbers = list(map(int, input("Enter numbers separated by spaces: ").split()))
+#Using Set() function
 
-#input("Enter numbers separated by spaces: "))).split() - ['10', '20', '30', '40']
-#map(int()) - map data to int 10, 20, 30, 40
-#list()  - to convert data to list
+# numbers = list(map(int, input("Enter numbers separated by spaces: ").split()))
 
-unique_numbers = list(set(numbers))  # Remove duplicates using set()
+# #input("Enter numbers separated by spaces: "))).split() - ['10', '20', '30', '40']
+# #map(int()) - map data to int 10, 20, 30, 40
+# #list()  - to convert data to list
 
-print(f"Original list is {numbers}")
-print(f"Unique list is {unique_numbers}")
+# unique_numbers = list(set(numbers))  # Remove duplicates using set()
+
+# print(f"Original list is {numbers}")
+# print(f"Unique list is {unique_numbers}")
+
+# 2 - Using Loop
+
+# numbers = list(map(int, input("Enter numbers separated by spaces: ").split()))
+
+# unique_number = []
+# for num in numbers:
+#     if num not in unique_number:
+#         unique_number.append(num)
+
+# print(f"Orginal List is {numbers}")
+# print(f"Unique List is {unique_number}")
+
+
