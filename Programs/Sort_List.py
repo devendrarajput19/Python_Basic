25e71d99adc6cbad6425f69b2f6e2921da9080b3
Devendra Rajput <devenrajput12@gmail.com>
2026-03-22 20:33:55 +0530

    Using Bubble sort algorithm

--- Programs/Sort_List.py
+++ Programs/Sort_List.py
@@ -1,8 +1,25 @@
 # 1 - Sort list using sort() function
 
+# numbers = list(map(int, input("Enter numbers separated by spaces: ").split()))
+
+# print(f"Original List is {numbers}")
+# numbers.sort()
+
+# print(f"Sorted List is {numbers}")
+
+# 2 - Using Bubble sort - Manual Algorithm
+
 numbers = list(map(int, input("Enter numbers separated by spaces: ").split()))
 
-print(f"Original List is {numbers}")
-numbers.sort()
+original = numbers.copy()
+n = len(numbers)
 
+#Bubble sort Algorithm
+
+for i in range(n):
+    for j in range(0, n-i-1):
+        if numbers[j] > numbers[j + 1]:
+            numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
+
+print(f"Original List is {original}")
 print(f"Sorted List is {numbers}")
