30da9e3f739789d8ce897a443379dd6085221e44
Devendra Rajput <devenrajput12@gmail.com>
2026-03-23 14:17:53 +0530

    merged list using extend() function

--- Programs/Merge_List.py
+++ Programs/Merge_List.py
@@ -1,11 +1,23 @@
 #1 - Merge list usng + operator
 
+# list1 = list(map(int, input("Enter 1st list with space: ").split()))
+# list2 = list(map(int, input("Enter 2nd list with space: ").split()))
+
+# merged = list1 + list2
+
+# print(f"1st List : {list1}")
+# print(f"2nd List : {list2}")
+
+# print(f"Merged list = {merged}")
+
+# 2 - Using extend() method
+
 list1 = list(map(int, input("Enter 1st list with space: ").split()))
 list2 = list(map(int, input("Enter 2nd list with space: ").split()))
 
-merged = list1 + list2
+merged = list1.copy()  
+list1.extend(list2)  #merged 2nd list to 1st list
 
-print(f"1st List : {list1}")
-print(f"2nd List : {list2}")
-
-print(f"Merged list = {merged}")
+print(f"1st List = {list1}")
+print(f"2nd List = {list2}")
+print(f"Merged list = {list1}")
