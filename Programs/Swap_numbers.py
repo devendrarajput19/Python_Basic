7870e94377b111b721062af451d18a6f033044f5
Devendra Rajput <devenrajput12@gmail.com>
2026-03-25 12:38:28 +0530

    Swap using Operators

--- Programs/Swap_numbers.py
+++ Programs/Swap_numbers.py
@@ -1,9 +1,23 @@
 # 1 - Swap using Tuple Unpacking
-a = int(input("Enter 1st number: "))
-b = int(input("Enter 2nd number: "))
+# a = int(input("Enter 1st number: "))
+# b = int(input("Enter 2nd number: "))
+
+# print(f"Before swap: a = {a}, b = {b}")
+
+# #swap using Tuple unpacking
+# a, b = b, a
+# print(f"After swap: a = {a}, b = {b}")
+
+# 2 - Using Arithmetic operation
+
+a = int(input("Enter 1st number = "))
+b = int(input("Enter 2nd number = "))
 
 print(f"Before swap: a = {a}, b = {b}")
 
-#swap using Tuple unpacking
-a, b = b, a
-print(f"After swap: a = {a}, b = {b}")
+#Swap using operators
+a = a + b
+b = a - b
+a = a - b
+
+print(f"After Swap a = {a}, b = {b}")
