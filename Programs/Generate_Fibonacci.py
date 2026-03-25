772fb85cddffd99876165818b4466ce52729bec1
Devendra Rajput <devenrajput12@gmail.com>
2026-03-25 12:55:52 +0530

    Generate Fibonacci using Recursion

--- Programs/Generate_Fibonacci.py
+++ Programs/Generate_Fibonacci.py
@@ -1,10 +1,27 @@
-n = int(input("Enter any number: "))
+# 1 - using recursion
+# n = int(input("Enter any number: "))
 
-a, b = 0, 1
-Fib_list = []
+# a, b = 0, 1
+# Fib_list = []
 
-for i in range(5):
-    Fib_list.append(a)
-    a, b = b, a + b
+# for i in range(5):
+#     Fib_list.append(a)
+#     a, b = b, a + b
 
-print(f"Fibonacci series = {Fib_list}")
+# print(f"Fibonacci series = {Fib_list}")
+
+# 2- using recursion
+
+def fibonacci(n):
+    if n <=0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+    
+n = int(input("Enter any number = "))
+
+fib_List = [fibonacci(i) for i in range(5)]
+
+print(f"Fibonacci series = {fib_List}")
