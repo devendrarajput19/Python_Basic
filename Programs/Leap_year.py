a87537550621f1450e8729c827646e0dcb3987b0
Devendra Rajput <devenrajput12@gmail.com>
2026-03-26 15:24:09 +0530

    Leap year using function

--- Programs/Leap_year.py
+++ Programs/Leap_year.py
@@ -1,8 +1,20 @@
 # 1 - To check leap year using if else
 
+# year = int(input("Enter any year: "))
+
+# if(year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+#     print(year, "is a Leap year")
+# else:
+#     print(year, "is not a Leap year")
+
+# 2 - Using function
+
+def leapyear(year):
+    return (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0)
+
 year = int(input("Enter any year: "))
 
-if(year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
-    print(year, "is a Leap year")
+if(leapyear(year)):
+    print(year, "is a leap year")
 else:
-    print(year, "is not a Leap year")
+    print(year, "is not a leap year")
