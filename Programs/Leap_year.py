946489f57409be5fc47d29c793b11dc8a50be916
Devendra Rajput <devenrajput12@gmail.com>
2026-03-26 15:20:58 +0530

    Leap Year using if else

---
+++ Programs/Leap_year.py
@@ -0,0 +1,8 @@
+# 1 - To check leap year using if else
+
+year = int(input("Enter any year: "))
+
+if(year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print(year, "is a Leap year")
+else:
+    print(year, "is not a Leap year")
