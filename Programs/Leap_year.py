083eb11cc396cbefef670acce9fcc0b8658a9522
Devendra Rajput <devenrajput12@gmail.com>
2026-03-26 15:26:16 +0530

    Leap year using Calender module

--- Programs/Leap_year.py
+++ Programs/Leap_year.py
@@ -9,12 +9,23 @@
 
 # 2 - Using function
 
-def leapyear(year):
-    return (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0)
+# def leapyear(year):
+#     return (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0)
+
+# year = int(input("Enter any year: "))
+
+# if(leapyear(year)):
+#     print(year, "is a leap year")
+# else:
+#     print(year, "is not a leap year")
+
+# 3 - using Calender module
+
+import calendar
 
 year = int(input("Enter any year: "))
 
-if(leapyear(year)):
-    print(year, "is a leap year")
+if(calendar.isleap(year)):
+    print(year, "is a Leap Year")
 else:
-    print(year, "is not a leap year")
+    print(year, "is not a Leap year")
