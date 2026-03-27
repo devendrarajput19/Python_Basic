55a01f124e1e2f5ce1b2e8b079da661ae5f2e015
Devendra Rajput <devenrajput12@gmail.com>
2026-03-27 11:46:43 +0530

    OOPS - 1st program - part 1

--- Programs/OOPS_1.py
+++ Programs/OOPS_1.py
@@ -1,12 +1,28 @@
+# class Student:
+
+#     def __init__(self, name, marks): #self is the default parameter we pass when we create constructor
+#         self.name = name 
+#         self.marks = marks
+#         print("Adding new student in Database") 
+
+# s1 = Student("Devendra", 97) #Every time object is created of class, it calls the constructor
+# print(s1.name, s1.marks)
+
+# s2 = Student("Ravi", 88)
+# print(s2.name, s2.marks)  #using self parameter we can access the class objects
+
 class Student:
-
-    def __init__(self, name, marks): #self is the default parameter we pass when we create constructor
-        self.name = name 
+    def __init__(self, name, marks):
+        self.name = name
         self.marks = marks
-        print("Adding new student in Database") 
 
-s1 = Student("Devendra", 97) #Every time object is created of class, it calls the constructor
-print(s1.name, s1.marks)
+    def get_avg(self):
+        sum = 0
+        for val in self.marks:
+            sum += val
+        print("Hi", self.name, "Your avg score is", sum/3)
 
-s2 = Student("Ravi", 88)
-print(s2.name, s2.marks)  #using self parameter we can access the class objects
+s1 = Student("Devendra", [92, 55, 66])
+s1.get_avg()    
+
+
