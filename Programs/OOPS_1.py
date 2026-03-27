42ae8c7b011cf2791ae98d09571f9c78821a8483
Devendra Rajput <devenrajput12@gmail.com>
2026-03-27 11:37:04 +0530

    OOPS - added other parameter

--- Programs/OOPS_1.py
+++ Programs/OOPS_1.py
@@ -1,8 +1,12 @@
 class Student:
 
-    def __init__(self, name): #self is the default parameter we pass when we create constructor
-        self.name = name #using self parameter we can access the class objects
+    def __init__(self, name, marks): #self is the default parameter we pass when we create constructor
+        self.name = name 
+        self.marks = marks
         print("Adding new student in Database") 
 
-s1 = Student("Devendra") #Every time object is created of class, it calls the constructor
-print(s1.name)
+s1 = Student("Devendra", 97) #Every time object is created of class, it calls the constructor
+print(s1.name, s1.marks)
+
+s2 = Student("Ravi", 88)
+print(s2.name, s2.marks)  #using self parameter we can access the class objects
