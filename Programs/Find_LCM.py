47d4c9ec7503f2965a63bbba0fee1837ff05ee96
Devendra Rajput <devenrajput12@gmail.com>
2026-03-30 15:20:24 +0530

    code format

--- Programs/Find_LCM.py
+++ Programs/Find_LCM.py
@@ -1,4 +1,4 @@
-# 1 - using GCD formula
+# 1 - using GCD formula - greatest comman divisor
 
 # a = int(input("Enter 1st number = "))
 # b = int(input("Enter 2nd number = "))
@@ -8,7 +8,7 @@
 #         x, y = y, x % y
 #     return x
 
-# gcd = find_gcd(a, b)
+# gcd = find_gcd(a, b)   
 # lcm = (a * b) // gcd
 
 # print("First number = ", a)
@@ -21,16 +21,15 @@
 a = int(input("Enter 1st number = "))  # a = 6
 b = int(input("Enter 2nd number = "))  # b = 4
 
-greater = max(a, b) #this will give max number then greater = 6
-lcm = greater  #lcm = 6
+greater = max(a, b)                    #this will give max number then greater = 6
+lcm = greater                          #lcm = 6
 
 while True:
     if lcm % a == 0 and lcm % b == 0:
         break
-    lcm += greater  # lcm = lcm + greater = lcm = 6 + 6, lcm = 12
+    lcm += greater                     #lcm = lcm + greater = lcm = 6 + 6, lcm = 12
 
 print("First number = ", a)
 print("Second number = ", b)
 print("LCM is ", lcm)
 
- 
