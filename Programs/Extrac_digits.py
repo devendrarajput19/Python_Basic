3336b4aaf9cae2eda5cf0038facab24cd095d971
Devendra Rajput <devenrajput12@gmail.com>
2026-04-02 15:30:04 +0530

    Get count of a number

--- Programs/Extrac_digits.py
+++ Programs/Extrac_digits.py
@@ -1,9 +1,24 @@
-def print_digits(n):
+# Extract digits of a number
+# def print_digits(n):
 
+#     while n > 0:
+#         digit = n % 10         #give last digit of a number
+#         print(f"Digit = {digit}")
+
+#         n = n // 10       # remove last digit of a number.
+
+# print_digits(5941)
+
+
+# To return the count of the digits.
+
+def count_digits(n):
+
+    count = 0
     while n > 0:
-        digit = n % 10
-        print(f"Digit = {digit}")
+        n = n // 10    # Remove the last digit
+        count += 1     # count is increased by 1 if any digit is found.
+    
+    return count
 
-        n = n // 10
-
-print_digits(5941)
+print(count_digits(6578))
