c1acdb8dc52b247b805e4eb0f57784ddd8addfd9
Devendra Rajput <devenrajput12@gmail.com>
2026-04-03 14:00:50 +0530

    Added - more basic programs

--- Programs/Check_number.py
+++ Programs/Check_number.py
@@ -20,13 +20,37 @@
 
 # 3 - Print even numbers from 1 to N
 
+# n = int(input("Enter any number = "))
+
+# for i in range(2, n + 1, 2):
+#     print(i)
+
+#     # OR
+
+# for i in range(2, n + 1):
+#     if i % 2 == 0:
+#         print(i)
+
+# 4 - Sum of N numbers
+
+# n = int(input("Enter any number = "))
+
+# total = 0
+# for i in range(1, n + 1):
+#     total += i
+
+# print(f"Sum of {n} = {total}")
+
+#5 - Check if a number is Perfect number - Sum of divisors should be equal to number
+
 n = int(input("Enter any number = "))
 
-for i in range(2, n + 1, 2):
-    print(i)
+total = 0
+for i in range(1, n):
+    if n % i == 0:
+        total += i
 
-    # OR
-
-for i in range(2, n + 1):
-    if i % 2 == 0:
-        print(i)
+if total == n:
+    print(f"{n} is a Perfect Number")
+else:
+    print(f"{n} is not a Perfect Number")
