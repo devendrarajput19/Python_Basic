17606b1d136a68a9d5a8d2df63326e0217855dd0
Devendra Rajput <devenrajput12@gmail.com>
2026-04-07 14:23:24 +0530

    Max and min number

--- Programs/Multiply_All_Element.py
+++ Programs/Multiply_All_Element.py
@@ -19,3 +19,15 @@ product = math.prod(numbers)  # Multiply all numbers in a list
 print(f"Original numbers = {numbers}")
 print(f"Multiplication of all numbers are {product}")
 
+# 3 - Find difference between max and min
+
+numbers = list(map(int, input("Enter numbers (space separated): ").split()))
+
+max_number = max(numbers)
+min_number = min(numbers)
+
+difference = max_number - min_number
+print(f"Numbers = {numbers}")
+print(f"Max number = {max_number}")
+print(f"Min number = {min_number}")
+print(f"Difference = {difference}")
