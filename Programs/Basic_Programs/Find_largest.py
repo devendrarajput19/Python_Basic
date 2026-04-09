73bae72e14280f158a3c2add968fe42cf72b262e
Devendra Rajput <devenrajput12@gmail.com>
2026-04-09 15:15:32 +0530

    largest number & Factorial of a num

--- Programs/Basic_Programs/Find_largest.py
+++ Programs/Basic_Programs/Find_largest.py
@@ -10,3 +10,20 @@ elif num2 >= num1 and num2 >= num3:
     print(f"{num2} is largest number...")
 else:
     print(f"{num3} is largest number...")
+
+# 2 - Using max function
+
+largest = max(num1, num2, num3)
+print(f"The largest number = {largest}")
+
+# 3 - Factorial of a number
+
+num = int(input("Enter any number = "))
+
+factorial = 1
+if num < 0:
+    print(f"Factorial cannot be defined for negative numbers...")
+else:
+    for i in range(1, num + 1):
+        factorial *= i
+    print(f"Factorial of {num} ={factorial}")
