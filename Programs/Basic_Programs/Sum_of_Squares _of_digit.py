4a5090ed54777c4e6ab8020227f6718d4324a3be
Devendra Rajput <devenrajput12@gmail.com>
2026-04-19 22:21:21 +0530

    Sum of Squares of digit

---
+++ Programs/Basic_Programs/Sum_of_Squares _of_digit.py
@@ -0,0 +1,12 @@
+# 1 - Sum of Squares usng loop
+
+num = input("Enter a number = ")
+
+sum_of_squares = 0
+
+for digit in num:
+    sum_of_squares += int(digit) ** 2
+
+print(f"Number = {num}")
+print(f"Digits = {' ,'.join(num)}")
+print(f"Sum of Squares     : {sum_of_squares}")
