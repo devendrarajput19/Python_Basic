66f53ffba2f9ff39ecceecce70eedde0ba4b0daf
Devendra Rajput <devenrajput12@gmail.com>
2026-05-25 22:10:26 +0530

    Leave Management System - Part 2

--- Programs/Intermediate Programs/Employee_leave_management.py
+++ Programs/Intermediate Programs/Employee_leave_management.py
@@ -47,7 +47,68 @@ class Employee:
     def get_leave_balance(self):
         """Return and display remaining leave balance."""
         print(f"\n📊 Leave Balance for {self.name}:")
-        print(f"   Total Leaves    : {Employee.TOTAL_LEAVES}")
-        print(f"   Used Leaves     : {Employee.TOTAL_LEAVES - self.leave_balance}")
+        print(f"   Total Leaves    : {Employee.Total_leaves}")
+        print(f"   Used Leaves     : {Employee.Total_leaves - self.leave_balance}")
         print(f"   Remaining       : {self.leave_balance}")
         return self.leave_balance
+    
+class Manager:
+    """Represents a manager who can approve or reject leave requests."""
+
+    def __init__(self, name):
+        self.name = name
+
+    def approve_leave(self, employee, request_id):
+        for req in employee.leave_requests:
+            if req["request_id"] == request_id:
+                if req["status"] == "Pending":
+                    print(f"Request #{request_id} is already '{req['status']}'.")
+
+                    return
+                req["status"] == "Approved"
+                employee.leave_balance -= req["days"]
+                print(f"\n Manager {self.name} APPROVED leave request #{request_id} for {employee.name}.")
+
+                print(f"   Remaining balance: {employee.leave_balance} days.")
+                return
+            
+        print(f"\n Request ID #{request_id} not found for {employee.name}.")
+
+
+if __name__ == "__main__":
+    print(" EMPLOYEE LEAVE MANAGEMENT SYSTEM - DEMO....")
+
+    emp1 = Employee(emp_id=101, name="Ravi", department="Engineering")
+    
+    mgr=Manager(name="Anil J")
+
+    emp1.apply_leave(
+        leave_type="Casual Leave",
+        start_date="2026-05-10",
+        end_date="2026-05-12",
+        days=3,
+        reason="Family function"
+    )
+    
+     # --- View All Leave Requests ---
+    print("\n STEP 2: View Leave Requests")
+    emp1.view_leave_requests()
+
+    # --- Manager Approves / Rejects ---
+
+    print("\n STEP 3: Manager Takes Action")
+    mgr.approve_leave(emp1, request_id=1)
+
+    # --- Check Leave Balance ---
+    print("\n STEP 4: Check Leave Balances")
+    emp1.get_leave_balance()
+        
+    # --- Edge Case: Apply more than balance ---
+    print("\n🔹 STEP 6: Edge Case - Apply Leave Beyond Balance")
+    emp1.apply_leave(
+        leave_type="Annual Leave",
+        start_date="2026-04-01",
+        end_date="2026-04-21",
+        days=21,
+        reason="Long holiday"
+    )
